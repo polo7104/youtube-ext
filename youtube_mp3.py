585ad390ed22fe09392da620400310485019ee48
jin lee <jinseoklee81@gmail.com>
2018-03-05 10:55:35 +0900

    first

--- youtube_mp3.py
+++ youtube_mp3.py
@@ -13,10 +13,10 @@ list = ["https://www.youtube.com/watch?v=JB-vcBj_X5o"]
 down_dir = "F:\음악\youtube-mp3"
 for i in range(len(list)):
     youtube = pytube.YouTube(list[i])
-    default_filename = youtube.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().default_filename
+    default_filename = youtube.streams.filter(progressive=True, file_extension="mp4").order_by("resolution").desc().first().default_filename
     if len(default_filename) > 0:
-        youtube.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(down_dir)
-        subprocess.call(['ffmpeg', '-i',  # cmd 명령어 수행
+        youtube.streams.filter(progressive=True, file_extension="mp4").order_by("resolution").desc().first().download(down_dir)
+        subprocess.call(["ffmpeg", "-i",  # cmd 명령어 수행
                          os.path.join(down_dir, default_filename),
                          os.path.join(down_dir, default_filename.replace("mp4","mp3"))
                          ])
